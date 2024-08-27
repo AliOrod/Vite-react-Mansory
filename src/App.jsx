ca7c91a52d8ba725c255550260e4ee23447b44e9
aliorod <127441824+AliOrod@users.noreply.github.com>
2024-08-28 00:56:52 +0330

    v 0.1.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import "./App.css";
-import { Button } from "flowbite-react";
-function App() {
+import Mansorylogo from "./assets/mansorylogo.svg";
+
+function Header() {
   return (
     <>
-      <Button>Click me</Button>;
+      <nav className="bg-[#2b2b2a] flex flex-wrap h-[3.5rem] justify-between">
+        <div className="left-menu "></div>
+        <div className="logo-container w-[210px] py-[13px] h-[57.6px] flex align-middle ">
+          <img src={Mansorylogo} alt="Logo" className="fill-white"></img>
+        </div>
+        <div className="right-menu "></div>
+      </nav>
     </>
   );
 }
 
-export default App;
+export default Header;
