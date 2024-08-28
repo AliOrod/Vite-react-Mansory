12ac6542d59e084525c4b2987459e06c4d4cf121
aliorod <127441824+AliOrod@users.noreply.github.com>
2024-08-29 02:07:51 +0330

    v 0.1.2
    Fonts added

--- src/Headermain.jsx
+++ src/Headermain.jsx
@@ -1,18 +1,20 @@
-import "./App.css";
+import "./Headermain.Module.css";
 import Mansorylogo from "./assets/mansorylogo.svg";
 
-function Header() {
+function Headermain() {
   return (
     <>
       <nav className="bg-[#2b2b2a] flex flex-wrap h-[3.5rem] justify-between">
-        <div className="left-menu "></div>
+        <div className="left-menu ">
+          <p className="font-[Fontello]">mansory</p>
+        </div>
         <div className="logo-container w-[210px] py-[13px] h-[57.6px] flex align-middle ">
           <img src={Mansorylogo} alt="Logo" className="fill-white"></img>
         </div>
-        <div className="right-menu "></div>
+        <div className="right-menu"></div>
       </nav>
     </>
   );
 }
 
-export default Header;
+export default Headermain;
